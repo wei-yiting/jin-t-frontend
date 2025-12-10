54f730f4eb227eb8a60738a34018eeb880ed7ece
Dong Wei (WYT) <wytdong@gmail.com>
2025-12-10 10:49:14 +0800

    chore: remove useTranscribe custom hook since its no longer used

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 import { useMediaRecorder } from "./useMediaRecorder";
 import { useAudioBlob } from "./useAudioBlob";
-import { useTranscribe } from "./useTranscribe";
 import { useClipboard } from "./useClipboard";
 
-export { useMediaRecorder, useAudioBlob, useTranscribe, useClipboard };
+export { useMediaRecorder, useAudioBlob, useClipboard };
